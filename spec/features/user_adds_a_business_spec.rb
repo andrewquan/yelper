d50622361add1848632d2ae4f67ee7f41f92d9d9
Andrew Quan <andrew.h.quan@gmail.com>
2016-01-10 18:35:58 -0800

    Added feature specs

---
+++ spec/features/user_adds_a_business_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+feature "user adds a business" do
+  scenario "user adds a new business" do
+    sign_in
+
+    visit_add_business_page
+
+    fill_in "Business Name", with: "Starbucks"
+    fill_in "Description", with: "Coffee shop."
+    select "Coffee", from: "Choose an image"
+    click_button "Register Business"
+    expect(page).to have_content "Starbucks"
+  end
+
+  def visit_add_business_page
+    find("a[href='/businesses/new']").click
+    expect(page).to have_content "Add New Business"
+  end
+end
---
+++ spec/features/user_reviews_a_business_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+feature "user reviews a business" do
+  scenario "user views a business page and leaves a review" do
+    chipotle = Business.create(name: "Chipotle", description: "Mexican food.", image_url: '/tmp/chipotle.jpg')
+    starbucks = Business.create(name: "Starbucks", description: "Coffee chain.", image_url: '/tmp/starbucks.jpg')
+    panda_express = Business.create(name: "Panda Express", description: "Chinese food.", image_url: '/tmp/panda_express.jpg')
+
+    sign_in
+
+    visit_business_page(chipotle)
+
+    submit_review("Awesome food!")
+  end
+
+  def visit_business_page(business)
+    find("a[href='/businesses/#{business.id}']").click
+    expect(page).to have_content business.name
+  end
+
+  def submit_review(content)
+    fill_in "review[content]", with: content
+    click_button "Submit"
+    expect(page).to have_content content
+  end
+end
